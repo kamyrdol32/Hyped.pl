cbaf5476670544afc2f8f66a275ca1767588831e
kamyrdol32 <kamyrdol32@gmail.com>
2022-07-23 00:21:39 +0200

    0.1.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import ThemeContext from "./Context/ThemeContext";
 import useLocalStorage from "use-local-storage";
 import useToken from "./JWT";
+import {Container} from "react-bootstrap";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // Components
 import MyNavBar from "./Components/Navbar/Navbar";
@@ -16,36 +20,53 @@ import MyProfile from "./Sites/Profile/MyProfile";
 
 // CSS
 import './App.css';
-import {Container} from "react-bootstrap";
 
 
 // Code
+const queryClient = new QueryClient()
+
 export default function App() {
+
+	// JWT
 	const { token, removeToken, setToken } = useToken();
 
 	// Dark mode
 	const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 	const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
 
+	// Root
 	const root = document.getElementById('root');
 	root.setAttribute("data-theme", theme);
 
 	return (
-		<ThemeContext.Provider value={{theme, setTheme}}>
-			<BrowserRouter>
-				<MyNavBar token={token}/>
-				<Container>
-					<Routes>
-						<Route path="/" element={<MyHome />} />
-						<Route path="/films" element={<MyFilmList />} />
-						<Route path="/film/:ID" element={<MyFilm />} />
+		<QueryClientProvider client={queryClient}>
+			<ThemeContext.Provider value={{theme, setTheme}}>
+				<BrowserRouter>
+					<MyNavBar token={token}/>
+					<Container>
+						<Routes>
+							<Route path="/" element={<MyHome />} />
+							<Route path="/films" element={<MyFilmList />} />
+							<Route path="/film/:ID" element={<MyFilm />} />
 
-						<Route path="/login" element={<MyLogin setToken={setToken} />} />
-						<Route path="/register" element={<MyRegister />} />
-						<Route path="/profile" element={<MyProfile token={token} setToken={setToken} removeToken={removeToken} />} />
-					</Routes>
-				</Container>
-			</BrowserRouter>
-		</ThemeContext.Provider>
+							<Route path="/login" element={<MyLogin setToken={setToken} />} />
+							<Route path="/register" element={<MyRegister />} />
+							<Route path="/profile" element={<MyProfile token={token} setToken={setToken} removeToken={removeToken} />} />
+						</Routes>
+					</Container>
+					<ToastContainer
+						position="bottom-right"
+						autoClose={5000}
+						hideProgressBar={false}
+						newestOnTop={false}
+						closeOnClick
+						rtl={false}
+						pauseOnFocusLoss
+						draggable
+						pauseOnHover
+					/>
+				</BrowserRouter>
+			</ThemeContext.Provider>
+		</QueryClientProvider>
 	)
 }
--- frontend/src/Sites/Profile/MyProfile.js
+++ frontend/src/Sites/Profile/MyProfile.js
@@ -1,111 +1,69 @@
 // Imports
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {Container} from "react-bootstrap";
-import 'react-toastify/dist/ReactToastify.css';
+import {useQuery} from "@tanstack/react-query";
 
 // CSS
 import './MyProfile.css';
-
-
-
-
-// Components
 import MyLoader from "../../Components/Loader/MyLoader";
+import {toast} from "react-toastify";
+import {useNavigate} from "react-router-dom";
+
 
 // Code
 export default function MyProfile(props) {
 
 	// States
-	const [username, setUsername] = useState('');
-	const [email, setEmail] = useState('');
+	const navigate = useNavigate();
 
-	const [error, setError] = useState(null);
-	const [isLoaded, setIsLoaded] = useState(false);
+	const [isLogged, setIsLogged] = useState(false);
+    const { isLoading, error, data } = useQuery(['Profile'], fetchProfile);
 
-	useEffect(() => {
-		getData()
-	}, [])
-
-	function getData() {
-		if (props.token) {
-			fetch('/auth/profile', {
-				method: 'GET',
-				headers: {
-					Authorization: 'Bearer ' + props.token
-				}
-			})
-				.then(res => res.json())
-				.then(
-					(result) => {
-						console.log(result);
-						result.access_token && props.setToken(result.access_token);
-
-						if (result.username && result.email) {
-							setUsername(result.username);
-							setEmail(result.email);
-							setIsLoaded(true);
-						} else {
-							setIsLoaded(false);
-						}
-					},
-					(error) => {
-						setError(error);
-					}
-				);
-		} else {
-			setTimeout(() => {
-				window.location.href = '/';
-			}, 3000);
-		}
-	}
-
-	function logout() {
-		fetch('/auth/logout', {
+	async function fetchProfile() {
+		const response = await fetch('/auth/profile', {
 			method: 'GET',
+			headers: {
+				Authorization: 'Bearer ' + props.token,
+				ContentType: 'application/json',
+			}
 		})
-			.then(res => res.json())
-			.then(
-				() => {
-					props.removeToken()
-				},
-				(error) => {
-					setError(error);
-					console.log(error);
-				}
-			);
+		if (response.status === 422 || response.status === 401) {
+			toast.error("Prosze sie zalogowac!");
+			navigate('/login');
+		} else {
+			setIsLogged(true);
+		}
+		return response.json();
 	}
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-        return <MyLoader />;
-    } else {
-	    return (
-		    <Container className="p-5 justify-content-center row">
-			    <Container id="MyProfile" className="col-md-8">
-				    <h1 className="MyProfile_Logo text-center"><span className="MyProfile_Logo fa fa-user-circle"/></h1>
-				    <h3 className="MyProfile_Header text-center">Mój profil</h3>
-				    <a onClick={logout}>Wyloguj</a>
-				    <h5>{error}</h5>
-				    <Container className="m-4">
-					    <div className="MyProfile_Info p-3">
-						    <div className="MyProfile_Info_Title">Username</div>
-						    <div className="MyProfile_Info_Value">{username}</div>
-					    </div>
-					    <div className="MyProfile_Info p-3">
-						    <div className="MyProfile_Info_Title">Adres e-mail</div>
-						    <div className="MyProfile_Info_Value">{email}</div>
-					    </div>
-					    <div className="MyProfile_Info p-3">
-						    <div className="MyProfile_Info_Title">Numer telefonu</div>
-						    <div className="MyProfile_Info_Value">123-456-789</div>
-					    </div>
-					    <div className="MyProfile_Info p-3">
-						    <div className="MyProfile_Info_Title">Adres</div>
-						    <div className="MyProfile_Info_Value">ul. Krakowska</div>
-					    </div>
-				    </Container>
-			    </Container>
-		    </Container>
-	    )
-    }
+
+	if (isLoading || !isLogged) return <MyLoader />
+	if (error) return toast.error(error);
+
+	return (
+		<Container className="p-5 justify-content-center row">
+			<Container id="MyProfile" className="col-md-8">
+				<h1 className="MyProfile_Logo text-center"><span className="MyProfile_Logo fa fa-user-circle"/></h1>
+				<h3 className="MyProfile_Header text-center">Mój profil</h3>
+				<button>Wyloguj</button>
+				<Container className="m-4">
+					<div className="MyProfile_Info p-3">
+						<div className="MyProfile_Info_Title">Username</div>
+						<div className="MyProfile_Info_Value">{data.username}</div>
+					</div>
+					<div className="MyProfile_Info p-3">
+						<div className="MyProfile_Info_Title">Adres e-mail</div>
+						<div className="MyProfile_Info_Value">{data.email}</div>
+					</div>
+					<div className="MyProfile_Info p-3">
+						<div className="MyProfile_Info_Title">Numer telefonu</div>
+						<div className="MyProfile_Info_Value">{data.phone}</div>
+					</div>
+					<div className="MyProfile_Info p-3">
+						<div className="MyProfile_Info_Title">Adres</div>
+						<div className="MyProfile_Info_Value">{data.adress}</div>
+					</div>
+				</Container>
+			</Container>
+		</Container>
+	)
 }
